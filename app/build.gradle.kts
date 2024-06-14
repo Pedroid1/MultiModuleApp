880db258c69b1de05f9a47b1a74594a0630f4411
Pedro Henrique de Souza Araujo <pedro.steam2016@hotmail.com>
2024-06-14 19:25:46 -0300

    refactor(app) removing dependencies from app build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,60 +38,15 @@ android {
 }
 
 dependencies {
-
-    implementation(project(":core:common"))
-    implementation(project(":core:data"))
-    implementation(project(":core:designsystem"))
-    implementation(project(":core:domain"))
-    implementation(project(":core:testing"))
-    implementation(project(":feature:onboarding"))
-    implementation(project(":feature:home"))
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit4)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(libs.truth)
-    androidTestImplementation(libs.truth)
-    testImplementation(libs.mockk)
-    androidTestImplementation (libs.mockk.android)
-    testImplementation(libs.turbine)
-    testImplementation(libs.androidx.core.testing)
-
-    // Hilt
-    implementation (libs.hilt.android)
-    kapt (libs.hilt.compiler)
-    androidTestImplementation  (libs.hilt.android.testing)
-    kaptAndroidTest (libs.hilt.compiler)
-    testImplementation (libs.hilt.android.testing)
-    kaptTest (libs.hilt.compiler)
-
-    // Lifecycle
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    kapt(libs.androidx.lifecycle.compiler)
-
-    // Coroutines
-    implementation(libs.kotlinx.coroutines.android)
-    testImplementation (libs.kotlinx.coroutines.test)
-
-    // Navigation Component
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    androidTestImplementation(libs.androidx.navigation.testing)
-
-    implementation(libs.lottie)
-
-    // Room
-    implementation(libs.androidx.room.runtime)
-    kapt(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
-    testImplementation(libs.androidx.room.testing)
+    val listExcludes = listOf(
+        ":core",
+        ":feature",
+        ":build-logic",
+        ":app"
+    )
+    rootProject.subprojects.forEach { module ->
+        if(module.path !in listExcludes) implementation(project(module.path))
+    }
 }
 
 kapt {
